fe50a2c6d729a3f2e9ef52aee867be71d2e6d0ce
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-11-16 15:09:57 +0800

    Update hook-package01.sh

--- scripts/immortalwrt/hook-package01.sh
+++ scripts/immortalwrt/hook-package01.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-# Set to local prepare
 
-# rtpengine
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/rtpengine/900-fix-linux-6.12-11.5.1.18.patch > customfeeds/telephony/net/rtpengine/patches/900-fix-linux-6.12-11.5.1.18.patch
-
-# routing - batman-adv fix build with linux-6.12
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/batman-adv/901-fix-linux-6.12rc2-builds.patch > customfeeds/routing/batman-adv/patches/901-fix-linux-6.12rc2-builds.patch
-
-# libxcrypt
-sed -i "/CONFIGURE_ARGS/i\TARGET_CFLAGS += -Wno-error=pedantic\n" package/libs/xcrypt/libxcrypt/Makefile
